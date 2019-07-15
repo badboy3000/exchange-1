6f6a8f6e0a59c6e635e529d2805a31d99bdf3ff6
fusheng <sysuyangkang@gmail.com>
2019-07-15 17:57:11 +0800

    add KlineDTO

---
+++ dtos/kline_dto.go
@@ -0,0 +1,12 @@
+package dtos
+
+import "github.com/shopspring/decimal"
+
+// KlineDTO is kline data for tradingview
+type KlineDTO struct {
+	Time  string          `json:"time"`
+	Open  decimal.Decimal `json:"open"`
+	High  decimal.Decimal `json:"high"`
+	Low   decimal.Decimal `json:"low"`
+	Close decimal.Decimal `json:"close"`
+}
